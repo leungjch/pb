db01337e6bc45812eeed6dee3004ee68d2fefeda
leungjch <justin.leung.ch@gmail.com>
2022-07-03 19:53:54 -0700

    update project structure

--- pb/py/setup.py
+++ pb/py/setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pb_leungjch',
     url='https://github.com/leungjch/pb',
     author='Justin Leung',
-    packages=['pb_leungjch'],
+    packages=find_packages(),
     install_requires=['grpcio-tools'],
     version='0.1',
     license='MIT'
